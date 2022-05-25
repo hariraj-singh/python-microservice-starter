27cb70b20bdaf986e12f3c6e3febebf831c03d6f
Hariraj Singh <harirajsingh@gmail.com>
2022-05-25 09:19:13 +0530

    Fixed the issue with Container error TypeError: 'type' object is not subscriptable

--- src/services/word_service.py
+++ src/services/word_service.py
@@ -3,6 +3,7 @@ This module represents a sample Servcie class
 """
 
 from models.sample_model import new_word_response
+from typing import Tuple
 
 word_list = []
 
@@ -15,7 +16,7 @@ class word_service():
     def __init__(self) -> None:
         pass
 
-    def detail(word: str) -> tuple[new_word_response, bool]:
+    def detail(word: str) -> Tuple[new_word_response, bool]:
         """Get detail for the given word
 
         Args:
